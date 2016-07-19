d8b71e7346756f076f469c8d6a056a786198d26c
Travis Rowland <travis.rowland@dynamictivity.com>
2016-07-18 17:45:43 -0700

    Got it working, but alpine linux doesn't support GLOB so I have to figure that one out; otherwise, we are unable to run migrations.

--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-rm -rf /www/vendors
+# Clear vendors (composer)
+rm -rf /www/vendor
+
+# Install app dependencies
 composer install --working-dir=/www
+
+# Copy over app configuration
 cp /app.php /www/config/app.php
+
+# Run db migrations
+cd /www; bin/cake migrations migrate
+
+# Seed the db
+cd /www; bin/cake migrations seed --seed DatabaseSeed
+
+# Start php-fpm
 /usr/bin/php-fpm

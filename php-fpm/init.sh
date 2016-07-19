bc5265ee64936ed09981d3ab9ba0bdf0203d9146
Travis Rowland <travis.rowland@dynamictivity.com>
2016-07-18 19:28:02 -0700

    And it works with Ubuntu! But again, it is not reading environment variables through php-fpm

--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clear vendors (composer)
-rm -rf /www/vendor
+#rm -rf /www/vendor
 
 # Install app dependencies
 composer install --working-dir=/www
@@ -9,11 +9,28 @@ composer install --working-dir=/www
 # Copy over app configuration
 cp /app.php /www/config/app.php
 
+# Wait for MySQL to come up (http://stackoverflow.com/questions/6118948/bash-loop-ping-successful)
+((count = 100000))                            # Maximum number to try.
+while [[ $count -ne 0 ]] ; do
+    nc -v db 3306                      # Try once.
+    rc=$?
+    if [[ $rc -eq 0 ]] ; then
+        ((count = 1))                      # If okay, flag to exit loop.
+    fi
+    ((count = count - 1))                  # So we don't go forever.
+done
+
+if [[ $rc -eq 0 ]] ; then                  # Make final determination.
+    echo 'The MySQL server is up.'
+else
+    echo 'Timeout waiting for MySQL server.'
+fi
+
 # Run db migrations
 cd /www; bin/cake migrations migrate
 
 # Seed the db
-cd /www; bin/cake migrations seed --seed DatabaseSeed
+cd /www; bin/cake migrations seed --seed DatabaseSeed || true
 
 # Start php-fpm
-/usr/bin/php-fpm
+/usr/sbin/php5-fpm
